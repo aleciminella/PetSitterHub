c75f86b2abf0088d84d61a63d322476e9d166ecb
Cosmin3333 <135050898+Cosmin3333@users.noreply.github.com>
2026-08-16 20:14:43 +0200

    Aggiunte card sitter di esempio

---
+++ frontend/js/search.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {   
+    $("#searchForm").on("submit", function (event) {     
+        event.preventDefault();     
+        console.log("Ricerca sitter inviata");   
+    });
+ }); 
