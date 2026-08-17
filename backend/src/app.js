02499b0e8bf1bca9b5d0f01939b2f2e1de8616bc
aleciminella <aleciminella@gmail.com>
2026-08-17 11:57:22 +0200

    Aggiunta registrazione utenti

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,4 +9,12 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/health", healthRoutes);
 
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  res.status(500).json({
+    error: "Internal server error"
+  });
+});
+
 module.exports = app;
