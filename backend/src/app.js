8ea23cfcb95794edb899e5264c15c96460960778
aleciminella <aleciminella@gmail.com>
2026-08-17 10:53:17 +0200

    Aggiunta struttura autenticazione

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const authRoutes = require("./routes/authRoutes");
 const healthRoutes = require("./routes/healthRoutes");
 
 const app = express();
 
 app.use(express.json());
 
+app.use("/api/auth", authRoutes);
 app.use("/api/health", healthRoutes);
 
 module.exports = app;
---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,9 @@
+function register(req, res) {
+  res.status(501).json({
+    error: "Register endpoint not implemented yet"
+  });
+}
+
+module.exports = {
+  register
+};
